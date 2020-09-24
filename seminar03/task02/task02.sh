7989dd4837e3d992f9c1cf4a8aa490b581ecd6d0
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-24 17:58:51 +0300

    Do Task2

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,17 @@
 #! /bin/bash
+n=$(($RANDOM * ($RANDOM / 1024)))
+isPrime=true
+div=1
+ while [[ $((div**2)) -le n ]];
+  do
+   if [ $((n % div)) -eq 0 ];
+   then
+    isPrime=false
+   fi
+   (( div+=1 ))
+  done
+echo $n $isPrime
+#function iFPrime(n) {
+#  if
+#}
+#iFPrime(n)
