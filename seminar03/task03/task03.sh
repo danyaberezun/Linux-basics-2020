64fb43dcaf468a6be76f0c6501206857e6f18d7f
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-24 18:16:02 +0300

    Do Task3

--- seminar03/task03/task03.sh
+++ seminar03/task03/task03.sh
@@ -1,1 +1,20 @@
 #! /bin/bash
+read -r digit1
+read -r digit2
+maxAns=1
+counter=1
+if [ $digit1 -le $digit2 ];
+then
+  min=$digit1
+else
+  min=digit2
+fi
+while [[ $counter -le $min ]];
+  do
+   if [ $((digit1 % counter)) -eq 0 ] && [ $((digit2 % counter)) -eq 0 ];
+   then
+    (( maxAns=counter))
+   fi
+   (( counter+=1 ))
+  done
+echo "$digit1 $digit2 $maxAns"
