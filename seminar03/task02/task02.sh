0f06109bb2eed64886802fe3cdc638e2c388fc14
smntin <smn.pankv@gmail.com>
2020-09-24 15:27:10 +0300

    Task 02

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,27 @@
 #! /bin/bash
+val=$(( $(( $RANDOM << 15 )) + $RANDOM ))
+(( val >>= 4 ))
+
+prime() {
+	if [[ $1 -lt 2 ]]; then
+		return 0
+	fi
+
+	for ((i = 2 ; $(( i * i )) < $1 ; i++)); do
+		if [[ $(( $1 % $i )) -eq 0 ]]; then
+			return 0
+		fi
+	done
+
+	return 1
+}
+
+prime $val
+res=$?
+
+if [[ $res -eq 1 ]]; then
+	echo "$val true"
+else
+	echo "$val false"
+fi
+
