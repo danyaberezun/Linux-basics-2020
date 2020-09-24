374be66a0462c3b5c187ff462fd7ce85224dcdb9
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-24 18:58:58 +0300

    Do Task4

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,15 @@
 #! /bin/bash
-
-echo 2 24 120
+#read -r digit1
+#read -r digit2
+#read -r digit3
+# shellcheck disable=SC2207
+arr=($(awk -F= '{print $1}' numbers.txt))
+len=${#arr[@]}
+for ((i = 0 ; i < len ; i++)); do
+  tempAns=1
+  for ((j = 1 ; j <= arr[i] ; j++)); do
+   (( tempAns=tempAns*j ))
+  done
+  echo $tempAns
+done
+echo "${arr[3]}"
