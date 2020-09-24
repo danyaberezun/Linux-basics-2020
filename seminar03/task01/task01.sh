f0c0df7734538a8a7f5e5d55c644a1f55094bcb9
Daniil Pavlenko <dp3481088@gmail.com>
2020-09-24 18:07:32 +0300

    solve 1 and 2

--- seminar03/task01/task01.sh
+++ seminar03/task01/task01.sh
@@ -1,1 +1,5 @@
-#!/bin/bash
+#! /bin/bash
+
+x=$(( RANDOM%(2 ** 15) ))
+dd if=/dev/random of=rnd.txt bs=1 count=$x
+echo $x
--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,24 @@
 #! /bin/bash
+
+function isprime {
+	n=$1
+	if [ $n -eq 1 ] || [ $n -eq 0 ]; then
+		return -1
+	fi
+
+	for ((i=2; i*i <= n; i++))
+	do
+		if [ $(( $n % $i )) -eq 0 ]; then
+			return -1
+		fi
+	done 
+	return 0
+}
+
+x=$(( RANDOM%(2 ** 15) ))
+if isprime $x;
+then
+	echo "$x true"
+else 
+	echo "$x false"
+fi
