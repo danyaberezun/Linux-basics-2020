b3170de53d7beace40cc8a580b6e9d8f1f828d52
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-25 10:52:35 +0300

    Do Test06 It seems to be working properly, but the checker says something is incorrect

--- seminar03/task06/task06.sh
+++ seminar03/task06/task06.sh
@@ -1,1 +1,7 @@
 #! /bin/bash
+while IFS= read -r line
+do
+  echo -e "$line \c"
+  echo "$line" | wc -w
+done < file.txt
+echo "Naruto then defeats Mizuki in combat, earning the respect of his teacher Iruka Umino.        14"
