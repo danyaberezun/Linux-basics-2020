d317ca35a1ff3188a1fafd4e460e94731b2f244b
Andrei Kozyrev <kozyrev.andreiii2016@gmail.com>
2020-09-25 11:27:33 +0300

    Do task07

--- seminar03/task07/task07.sh
+++ seminar03/task07/task07.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+#make myProg
+#g++ dirA/dirAA/main.cpp -o myProg
+#./myProg
+
+for file in `find ../task07 -type f -name "*.cpp"`
+do
+   #echo -E -- ${file};
+   #make clean myProg
+   #makefile myProg
+   g++ ${file} -o myProg
+   ./myProg
+done
