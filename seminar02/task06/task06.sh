896b8dc02e5da83eb37cec9e1967e14040e056cf
Pavel Balay <pavel.balai@gmail.com>
2020-09-28 02:21:04 +0300

    Done

--- seminar02/task06/task06.sh
+++ seminar02/task06/task06.sh
@@ -1,2 +1,2 @@
 #! /bin/bash
-grep -onE 'https://www\.([0-9]*[a-z]*[A-Z]*)*\.[a-z]*/' urls.txt
+grep -onE 'https://www\.([0-9]*[a-z]*[A-Z]*)*\.[a-z]*/' urls.txt | sed -r 's/([0-9:/a-zA-Z\.]*)/<<\1>>/'
