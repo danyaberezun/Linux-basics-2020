b224a8ee60f63c1de3738a6e34471a01e38a29bd
Mark Prudnikov <mark.prudnikov@inbox.ru>
2020-09-30 16:51:21 +0300

    Task 06 done

--- seminar03/task06/task06.sh
+++ seminar03/task06/task06.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+
+# Читать текст из заданного файла и выводить построчно на stdout,
+# добавляя в конец каждой строки количество слов в ней (можно получить связыванием в конвейер команд echo и wc).
+
+file=file.txt
+cat $file | while read -r line
+do
+echo -n "$line"
+echo -n "$line" | wc -w
+done
