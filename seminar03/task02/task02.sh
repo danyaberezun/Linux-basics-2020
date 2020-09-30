855d16c3076175b5b30cb90b525cd3c50b615969
Mark Prudnikov <mark.prudnikov@inbox.ru>
2020-09-30 16:08:03 +0300

    Second task done

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+first=$RANDOM
+second=$RANDOM
+second=$(( first / 1024))
+integer=$(( first * second ))
+echo $integer
+i=2
+j=0
+while [ $integer -ge $(( i*i )) ] && [ $j -ne 1 ]
+do
+if [ $(( integer % i )) -eq 0 ]
+then
+j=1
+else
+i=$(( i + 1 ))
+fi
+done
+if [ $j -eq 1 ]
+then
+echo false
+else
+echo true
+fi
