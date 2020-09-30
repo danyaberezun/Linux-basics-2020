1de006b0a68e1d59586adeccd1a5fa3e1500a4fe
Mark Prudnikov <mark.prudnikov@inbox.ru>
2020-09-30 14:55:09 +0300

    Task04 done.

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,23 @@
 #! /bin/bash
-
-echo 2 24 120
+FILE=numbers.txt
+{
+read line
+} < $FILE
+a=`echo $line | cut -f1 -d' '`
+b=`echo $line | cut -f2 -d' '`
+c=`echo $line | cut -f3 -d' '`
+function factorial {
+result=1
+local temp=$1
+while [ $temp -gt 0 ]
+do
+result=$(( $result * $temp ))
+temp=$(( $temp - 1 ))
+done
+}
+factorial $a
+echo -n "$result "
+factorial $b
+echo -n "$result "
+factorial $c
+echo $result
