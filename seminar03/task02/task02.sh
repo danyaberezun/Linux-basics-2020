ed3dbd8ad7a85f3586460261d7f5bba61d13f4f2
TheGeode <a.golovastenko.a@gmail.com>
2020-09-30 13:51:03 +0000

    Add the second task solution

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,12 @@
 #! /bin/bash
+number=$(($RANDOM * 32 + $RANDOM % 32))
+prime=true
+for ((i = 2; i < 1024; i++))
+do
+	if ((number % i == 0))
+	then
+		prime=false
+		break
+	fi
+done
+echo $number $prime
