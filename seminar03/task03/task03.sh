e7cf4d43b00dd953f2288afc301d0be1a88a51fa
Mark Prudnikov <mark.prudnikov@inbox.ru>
2020-09-30 14:09:23 +0300

    Task03 done

--- seminar03/task03/task03.sh
+++ seminar03/task03/task03.sh
@@ -1,1 +1,14 @@
 #! /bin/bash
+read -r a b
+echo -n "$a "
+echo -n "$b "
+while [ $a -gt 0 ] && [ $b -gt 0 ]
+do
+if [ $a -gt $b ]
+then
+a=$(($a % $b))
+else
+b=$(($b % $a))
+fi
+done
+echo $((a + b))
