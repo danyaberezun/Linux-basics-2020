51aaeb9d7e075391c00efd5c53dcf2f91a176977
IDKWNTCMF <71064019+IDKWNTCMF@users.noreply.github.com>
2020-10-04 19:34:55 +0300

    Update task02.sh

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,18 @@
 #! /bin/bash
+s=$RANDOM 
+let "s <<= 5"
+let "s = s + $RANDOM % 32"
+i=2
+until [ "$i" -eq 1024 ]
+do
+if (( s % i == 0 ))
+then
+echo "$s false"
+break
+fi
+let "i = i + 1"
+done
+if [ $i -eq 1024 ]
+then
+echo "$s true"
+fi
