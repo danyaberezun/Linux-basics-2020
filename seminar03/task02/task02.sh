46c9d013bfa994da301f410be01fb942a211c5c5
Arseny Yusufov <senya.yusufov@yandex.ru>
2020-10-08 21:42:57 +0300

    add task02

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,20 @@
 #! /bin/bash
+
+number=$RANDOM
+
+sqrtnumber=$number 
+
+# let number=number%10
+
+answer="$number true"
+
+for i in $(seq 2 1 $(($number-1)))
+do
+  if [ "$(($number % $i))" -eq "0" ]
+  then
+    answer="$number false"
+    break
+  fi
+done
+
+echo $answer
