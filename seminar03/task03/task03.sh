cf293c3a4adc7c98a6210d8b07c2522f8aa01218
Arseny Yusufov <senya.yusufov@yandex.ru>
2020-10-08 23:41:53 +0300

    add task03; NOTE THAT CHECKER DOSEN'T WORK

--- seminar03/task03/task03.sh
+++ seminar03/task03/task03.sh
@@ -1,1 +1,18 @@
 #! /bin/bash
+
+read -r num1 num2
+
+echo -n "$num1 "
+echo -n "$num2 "
+
+while [ $num1 -ne $num2 ]
+do
+  if [ $num1 -gt $num2 ]
+  then
+    num1=$(($num1-$num2))
+  else
+    num2=$(($num2-$num1))
+  fi
+done
+
+echo $num1
