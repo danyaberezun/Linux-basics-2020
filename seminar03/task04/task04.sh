041547f983ee4a44f76fd8d4e9fbde54b4b79dff
Arseny Yusufov <senya.yusufov@yandex.ru>
2020-10-09 15:29:23 +0300

    add task04

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,38 @@
 #! /bin/bash
 
-echo 2 24 120
+function fac() {
+  num=1
+
+  for i in $(seq 1 1 $1); do
+    num=$(($num*$i))
+  done
+
+  echo $num
+  return $num
+}
+
+arr=()
+i=0
+
+read line < "numbers.txt"
+len=$(echo $line | wc -c)
+let len=len/2
+
+for i in $(seq 1 1 $len); do
+  arr[$i]=$(echo $line | cut -d" " -f $i)
+done
+
+#  UNCOMMENT IF NUMBERS.TXT HAS MORE THAT 1 LINE
+
+# while read line; do
+#   arr[$i]
+#   echo $line
+
+#   i=$($i+1)
+# done < "numbers.txt"
+
+for item in ${arr[*]}
+do
+  val=$(fac $item)
+  echo -n "$val "
+done 
