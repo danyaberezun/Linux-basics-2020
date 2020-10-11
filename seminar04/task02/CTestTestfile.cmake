e329abef336e94b629571501b0bd41602ebec173
Stephen Ostapenko <stepanos2002@gmail.com>
2020-10-11 20:16:47 +0300

    deleted all the shit from commit

--- seminar04/task02/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/stephen/linprac/Linux-basics-2020/seminar04/task02
-# Build directory: /home/stephen/linprac/Linux-basics-2020/seminar04/task02
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(tests "bash" "run_tests.sh")
