01886660616ae8a562378010be55c3ae17747f86
Albert Skalt <allber971@gmail.com>
2020-10-15 00:26:44 +0300

    add hw4

---
+++ seminar04/task02/CTestTestfile.cmake
@@ -0,0 +1,14 @@
+# CMake generated Testfile for 
+# Source directory: /home/arrias/Projects/Linux-basics-2020/seminar04/task02
+# Build directory: /home/arrias/Projects/Linux-basics-2020/seminar04/task02
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "main" "0")
+set_tests_properties(test1 PROPERTIES  PASS_REGULAR_EXPRESSION "0" _BACKTRACE_TRIPLES "/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;12;test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
+add_test(test2 "main" "1")
+set_tests_properties(test2 PROPERTIES  PASS_REGULAR_EXPRESSION "1" _BACKTRACE_TRIPLES "/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;13;test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
+add_test(test3 "main" "2")
+set_tests_properties(test3 PROPERTIES  PASS_REGULAR_EXPRESSION "1" _BACKTRACE_TRIPLES "/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;14;test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
+add_test(test4 "main" "3")
+set_tests_properties(test4 PROPERTIES  PASS_REGULAR_EXPRESSION "2" _BACKTRACE_TRIPLES "/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;15;test;/home/arrias/Projects/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
