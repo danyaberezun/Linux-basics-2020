c841dfe8f152f08180d5daca1a6407df7221edea
Alyona <alyonacherepanova6954@gmail.com>
2020-10-14 23:40:53 +0300

    14.10

---
+++ seminar04/task02/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/monic/Linux(I_semestr)/Linux-basics-2020/seminar04/task02
+# Build directory: /home/monic/Linux(I_semestr)/Linux-basics-2020/seminar04/task02
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(KEK "bash" "test.sh")
+set_tests_properties(KEK PROPERTIES  _BACKTRACE_TRIPLES "/home/monic/Linux(I_semestr)/Linux-basics-2020/seminar04/task02/CMakeLists.txt;10;add_test;/home/monic/Linux(I_semestr)/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
---
+++ seminar04/task02/test.sh
@@ -0,0 +1,1 @@
+diff <(echo 5) <(./fast 5)
