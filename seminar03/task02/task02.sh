5e97cbe6e4f5caa541b44e3767aa2807fd584741
Jacob Payne <jacob@daohub.io>
2020-10-21 19:11:11 +0300

    Completed task02

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,11 @@
 #! /bin/bash
+r=$(awk -v min=0 -v max=1048576 'BEGIN{srand(); print int(min+rand()*(max-min+1))}')
+for((i=2; i<=r/2; i++))
+do
+  if [ $((r%i)) -eq 0 ]
+  then
+    echo "$r false"
+    exit
+  fi
+done
+echo "$r true"
