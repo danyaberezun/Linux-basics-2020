552b1c86910bccb1a6135ea130b6109f6e4de68f
mikrsie2 <mikrise2@mail.ru>
2020-10-21 06:20:11 +0300

    all is completed

---
+++ seminar04/task02/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: /home/mikrise2/Linux-basics-2020/seminar04/task02
+# Build directory: /home/mikrise2/Linux-basics-2020/seminar04/task02
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test1 "main" "0")
+set_tests_properties(test1 PROPERTIES  PASS_REGULAR_EXPRESSION "0" _BACKTRACE_TRIPLES "/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;12;testing;/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
+add_test(test2 "main" "1")
+set_tests_properties(test2 PROPERTIES  PASS_REGULAR_EXPRESSION "1" _BACKTRACE_TRIPLES "/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;8;add_test;/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;13;testing;/home/mikrise2/Linux-basics-2020/seminar04/task02/CMakeLists.txt;0;")
