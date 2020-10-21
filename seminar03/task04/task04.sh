a77c6a1757df8236ce9847cae7da0235e118dc88
Jacob Payne <jacob@daohub.io>
2020-10-21 20:09:00 +0300

    Complete task04

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,11 @@
 #! /bin/bash
-
-echo 2 24 120
+for a in $(cat numbers.txt); do
+    i=$((1))
+    r=$((1))
+    while [ $(($i < $a)) == 1 ]; do
+        i=$(($i + 1))
+        r=$(($r * $i))
+    done
+    "echo" "-ne" "$r "
+done
+echo
