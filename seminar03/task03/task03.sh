d5f286c1ec86f90e0db59fcd0673f66670d3578b
Jacob Payne <jacob@daohub.io>
2020-10-21 19:33:57 +0300

    Complete task03

--- seminar03/task03/task03.sh
+++ seminar03/task03/task03.sh
@@ -1,1 +1,24 @@
 #! /bin/bash
+function gcd {
+  local b
+  local a
+  a="$1"
+  b="$2"
+  if [ "$b" == $((0)) ]; then
+    "echo" "-ne" "$a"
+    return  
+fi
+  if [ "$a" == $((0)) ]; then
+    "echo" "-ne" "$b"
+    return  
+fi
+  if [ $(($a > $b)) == 1 ]; then
+    "echo" "-ne" $("gcd" "$b" $(($a % $b)))
+    return
+  else
+    "echo" "-ne" $("gcd" "$a" $(($b % $a)))
+    return
+  fi
+}
+
+echo $1 $2 $(gcd $1 $2)
