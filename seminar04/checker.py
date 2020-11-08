085428393b8533d7d36561fa58162cc33399fd28
Yurarara-ed <st091607@gmail.com>
2020-11-09 01:25:09 +0500

    cOrReCtIoNs

--- seminar04/checker.py
+++ seminar04/checker.py
@@ -5,10 +5,10 @@ import re
 def findWholeWord(w):
     return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
 
-TASK_COUNT = 7
+TASK_COUNT = 2
 completed_taks = 0
 
-for i in range(1, TASK_COUNT):
+for i in range(1, TASK_COUNT +1):
     os.chdir(f'task0{i}/')
     p = subprocess.Popen(['python3', f'test0{i}.py'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     stdout,stderr = p.communicate()
