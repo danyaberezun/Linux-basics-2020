ddb7190314cea0bb5395faff902525c80bb4c7f7
emiliya16 <71070803+emiliya16@users.noreply.github.com>
2020-11-11 20:16:20 +0300

    Update task02.sh

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,1 +1,23 @@
 #! /bin/bash
+fNumber=$RANDOM
+sNumber=$RANDOM
+sNumber=$((fNumber/1024))
+int=$((fNumber*snumber))
+echo $int
+ i=0
+ j=2
+while [ $int -ge $(( i*i)) ] && [ $j -ne 1 ]
+	do
+		if [ $(( $int % i )) -eq 0 ]
+then
+j=1
+else
+i=$(( i+1 ))
+    fi
+done
+    if [ $j -eq 1 ]
+then
+			echo false
+		else
+			echo true
+		fi
