29a0af50079f51f65ae774dd0195f93e8790815d
VagifBaratow <71065168+VagifBaratow@users.noreply.github.com>
2020-11-12 13:21:33 +0500

    Update task02.sh

--- seminar03/task02/task02.sh
+++ seminar03/task02/task02.sh
@@ -1,22 +1,7 @@
 #! /bin/bash
-Num1=$RANDOM
-Num2=$((Num1/1024))
-int=$((Num1*Num2))
-echo $int
-a=0
-b=2
-while [ $int -ge $(( a*a)) ] && [ $b -ne 1 ]
-	do
-		if [ $(( $int % a )) -eq 0 ]
-then
-b=1
-else
-a=$(( a+1 ))
-    fi
-done
-    if [ $b -eq 1 ]
-then
-			echo false
-		else
-			echo true
-		fi
+Nsz=$RANDOM
+touch rnd.txt
+
+head -c $sz /dev/urandom >> rnd.txt
+
+echo $sz
