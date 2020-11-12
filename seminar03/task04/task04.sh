fd86048b01401876d8a35cd3f3fe3cd157bcc859
VagifBaratow <71065168+VagifBaratow@users.noreply.github.com>
2020-11-12 13:28:25 +0500

    Update task04.sh

--- seminar03/task04/task04.sh
+++ seminar03/task04/task04.sh
@@ -1,3 +1,22 @@
 #! /bin/bash
+file="numbers.txt"
 
-echo 2 24 120
+while read line  || [ -n "$line" ];
+
+do
+
+    for x in $line;
+
+    do
+
+        res=1
+
+        for ((i=2; i <= x; ++i)); do
+		res=$(( res * i ))
+	done
+
+        printf "$res "
+
+    done
+
+done < "$file"
