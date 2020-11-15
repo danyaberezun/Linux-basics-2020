dd165285a6124a833c43fc50f485732660ce1a22
emiliya16 <71070803+emiliya16@users.noreply.github.com>
2020-11-15 17:48:58 +0300

    Create DependInfo.cmake

---
+++ seminar04/task02/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,16 @@
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/emiliya16/Desktop/linuxsecond/Linux-basics-2020/seminar04/task02/src/fast.cpp" "/home/emiliya16/Desktop/linuxsecond/Linux-basics-2020/seminar04/task02/CMakeFiles/main.dir/src/fast.cpp.o"
+  "/home/emiliya16/Desktop/linuxsecond/Linux-basics-2020/seminar04/task02/src/main.cpp" "/home/emiliya16/Desktop/linuxsecond/Linux-basics-2020/seminar04/task02/CMakeFiles/main.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+  
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
