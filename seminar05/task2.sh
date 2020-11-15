1aab64de6f6d0f84f2b8e6c88695bec21b48b61f
mrlolthe1st <ivningman@gmail.com>
2020-11-15 12:41:34 +0300

    Hw5 Novozhilov Aleksandr

--- seminar05/task2.sh
+++ seminar05/task2.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+print_it="0"
+while IFS= read -r line
+do
+	read -a xx <<< "$line"
+	if [[ $1 == ${xx[0]} ]]; then
+		print_it="1"
+	fi
+	if [[ $print_it == "1" ]]; then
+		vr=`cut -c 9- <<< "$line"`
+		echo $vr
+	fi
+	if [[ $2 == ${xx[0]} ]]; then
+		print_it="0"
+	fi
+	
+done <<< $(git log --pretty=format:"%h %s")
