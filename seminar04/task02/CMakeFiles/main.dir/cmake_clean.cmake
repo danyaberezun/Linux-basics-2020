2605c8b379e86d121c3cb5c72cb650881857d6bd
emiliya16 <71070803+emiliya16@users.noreply.github.com>
2020-11-15 17:40:51 +0300

    Create cmake_clean.cmake

---
+++ seminar04/task02/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/fast.cpp.o"
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "main"
+  "main.pdb"
+)
+
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
